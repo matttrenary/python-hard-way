3f41c84927ab1539ceeb9227100a20ec006b0001
Matt Trenary <matttrenary@gmail.com>
2017-06-24 22:08:14 -0400

    Ex12 in 3.5 based on 3.6 tutorial

--- ex12.py
+++ ex12.py
@@ -5,5 +5,10 @@ height = input("How tall are you? ")
 weight = input("How much do you weigh? ")
 
 # Print raw to see example ' and " in height
-print ("So, you're %r old, %r tall, and %r heavy." % 
-       (age, height, weight))
+print ("So, you're {} old, {} tall, and {} heavy.".format(
+       age, height, weight))
+
+# Trying to make the switch to Python the Hard Way 3
+# But it uses 3.6 and I'm not able to get that
+# So will try to do some 3.5 things to make it work
+# Like the .format above
