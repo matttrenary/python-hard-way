60426071f3b1a9636f97a2667fc398b7408e8c2f
Matt Trenary <matttrenary@gmail.com>
2017-06-24 22:07:35 -0400

    Tried to rework for 3.6 but not complete

--- ex5.py
+++ ex5.py
@@ -1,4 +1,21 @@
 my_name = 'Zed A. Shaw'
-weight = 35
+my_age = 35
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
 
-print ("Let's talk about %s, he weights %d." % (my_name, weight))
+print('stuff')
+print("stuff")
+print(my_name)
+
+print("Let's talk about {my_name}.")
+print("He's {my_height} inches tall.")
+print("He's {my_weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print("He's got {my_eyes} eyes and {my_hair} hair.")
+print("His teeth are usually {my_teeth} depending on the coffee.")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
